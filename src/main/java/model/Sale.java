b5f9687d63783bb9e6af52800f6845e910c7cfab
Usa <usa@Totoro.local>
2018-01-18 15:01:29 +0700

    Add Sale class

---
+++ src/main/java/model/Sale.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sale {
+    private List<SalesLineItem> lineItems;
+    private LocalDateTime date;
+    private boolean isComplete;
+
+    public Sale() {
+        lineItems = new ArrayList<SalesLineItem>();
+        date = LocalDateTime.now();
+        isComplete = false;
+    }
+    
+}
